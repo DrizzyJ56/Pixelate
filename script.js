2ae1becc18971228f77e6636fa3db22b13637f43
DrizzyJ56 <blevins.j921@gmail.com>
2022-10-03 11:22:15 -0400

    Making Rows and start colorize

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your code here
+const tableGrab = document.getElementById('table')
+
+function makeRow(){
+    const tr = document.createElement('tr')
+    for(let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        tr.appendChild(td)
+    }    
+    tableGrab.appendChild(tr)
+}
+makeRow()
+
+const addRow = document.getElementById('add-row')
+addRow.addEventListener('click', makeRow)
+
+tableGrab.addEventListener('click', colorize)
+
+function colorize(event){
+    console.log('clicked!')
+}
+
+
